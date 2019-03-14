29c1aa32101945a20457bc37b3a791a1daf7d362
Tyler1551 <46712398+Tyler1551@users.noreply.github.com>
2019-03-14 14:22:59 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct board()
+{
+	int board [3][3] = {
+		{7, 8, 9},
+		{4, 5, 6},
+		{1, 2, 3}
+	};
+}
+
+int main(int argc, char const *argv[])
+{
+	init_board();
+	printf("Board Initiated\n");
+	
+	for(int i = 0; i < )
+
+
+	return 0;
+}
