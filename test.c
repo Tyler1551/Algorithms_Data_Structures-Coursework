f314451eb8d48a0be59314ef95eed8d116007141
tyler <kakuja16@gmail.com>
2019-03-21 16:22:21 +0000

    Insert working, changed from 2D array to a single

--- test.c
+++ test.c
@@ -2,67 +2,79 @@
 
 typedef enum { false, true } bool;
 
+#define MAX 9
+
+char input = 'X';
+
 //shows intial board with positions labelled
 void init_board()
 {
-	char board [3][3] = {
-		board[0][0] = 7, board[0][1] = 8, board[0][2] = 9,
-		board[1][0] = 4, board[1][1] = 5, board[1][2] = 6,
-		board[2][0] = 1, board[2][1] = 2, board[2][2] = 3	
-	};
+	char board [MAX] = {1,2,3,4,5,6,7,8,9};
 
-	for(int row = 0; row < 3; row++)
-	{
-		for(int col = 0; col < 3; col++)
-		{
-			printf("|%d|", board[row][col]);
-		}
-		printf("\n");
-	}	
+		printf("| %d || %d || %d |\n", board[0], board[1], board[2]);
+		printf("| %d || %d || %d |\n", board[3], board[4], board[5]);
+		printf("| %d || %d || %d |\n", board[6], board[7], board[8]);
 }
 
 //shows game board that the user makes moves on
-void display_board(char game_board[3][3])
+void display_board(char game_board[9])
 {
 	printf("\n\n");
 
-	for(int row = 0; row < 3; row++)
-	{
-		for(int col = 0; col < 3; col++)
-		{
-			printf("| %c |", game_board[row][col]);
-		}
-		printf("\n");
-	}
+		printf("| %c || %c || %c |\n", game_board[0], game_board[1], game_board[2]);
+		printf("-------------\n");
+		printf("| %c || %c || %c |\n", game_board[3], game_board[4], game_board[5]);
+		printf("-------------\n");
+		printf("| %c || %c || %c |\n", game_board[6], game_board[7], game_board[8]);
 }
 
 //sets all positons to blank
-void init_game_board(char game_board[3][3])
+void init_game_board(char game_board[9])
 {
-	for(int row = 0; row < 3; row++)
+	int i; 
+	for(i = 0; i < MAX; i++)
 	{
-		for(int col = 0; col < 3; col++)
-		{
-			game_board[row][col] = 0;
-		}
+		game_board[i] = 0;
 	}
 }
 
-void insert(char game_board, int pos, char symbol)
+void insert(char* game_board)
 {
-	
+	int pos;
+	input = 'X';
+	scanf("%d", &pos);
+	game_board[pos] = input;
+}
+
+void switch_char()
+{
+	if(input = 'X')
+	{
+		input = 'O';
+	}
+	else if(input = 'O')
+	{
+		input = 'X';
+	}
 }
 
 int main(int argc, char const *argv[])
 {
+	bool gamewon = false;
+
 	printf("\t\t\t\tWelcome to naughts and crosses!\n\t\t\tTo play choose a position on the grid using the numpad\n\t\tThe first grid shown lists the positions of the grid to play\n\n");
 	init_board();
-	char game_board [3][3];
+	char game_board [MAX];
 	init_game_board(game_board);
-	insert(game_board, 8, 'X');
 	display_board(game_board);
-	
 
+	while(gamewon != true)
+	{
+		insert(game_board);
+		display_board(game_board);
+		switch_char();
+
+	}
 
 	return 0;
 }
